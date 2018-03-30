dca2ab893534a849951f048855a05619657df7cd
Adrian Thompson <adrian.thompson@sky.uk>
2018-03-30 20:47:59 +0100

    first commit

---
+++ src/App/Header/header-names.js
@@ -0,0 +1,9 @@
+export const headerNames = {
+    sections: [
+        { name: 'home', link: '#' },
+        { name: 'about', link: '#' },
+        { name: 'media', link: '#' },
+        { name: 'gallery', link: '#' },
+        { name: 'contact', link: '#' }
+    ]
+}
---
+++ src/App/Header/header.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { headerNames } from './header-names';
+import hamburger from './hamburger.png';
+import styles from './styles.css';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMobile: false,
+            isDesktop: true,
+            navOpen: false,
+        }
+    }
+
+    openMobNav = () => {
+        this.setState({
+            isMobile: true,
+            isDesktop: false,
+            navOpen: !this.state.navOpen,
+        })
+    }
+
+    render() {
+        const { navOpen } = this.state;
+        return(
+            <div className="container">
+                <img 
+                    className="hamburger" 
+                    onClick={this.openMobNav}
+                    src={hamburger} 
+                    width={30} 
+                    height={30} 
+                    />
+                { navOpen ? 
+                    <ul className="resp-nav">{headerNames.sections.map((each, i) => 
+                        <li className="resp-navbar-item" key={i}>{each.name}</li>)}
+                    </ul>
+                    : null 
+                }
+                {headerNames.sections.map((each, i) => {
+                    return <a href={each.link} key={i} className="navbar-item">{each.name}</a>
+                })}
+            </div>
+        )
+    }
+};
+
+export default Header;
