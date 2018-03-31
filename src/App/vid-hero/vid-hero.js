5aa328fd39d17ed856f792e77929aa0e1ad7ccd5
Adrian Thompson <adrian.thompson@sky.uk>
2018-03-31 11:56:38 +0100

    vid placeholders & resp width

--- src/App/vid-hero/vid-hero.js
+++ src/App/vid-hero/vid-hero.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import videoPlaceholder from './videoPlaceholder.jpg'
 import styles from './styles.css';
 
 class VideoSectionContainer extends Component {
@@ -20,15 +21,15 @@ class VideoSectionContainer extends Component {
 
                 <div className="vid-box-container">
                     <div className="vid-box">
-                        <img className="vid-box-player" width={400} height={200 }/>
+                        <img src={videoPlaceholder} className="vid-box-player" width={400} height={200 }/>
                         <h2 className="vid-box-title">Title no 1</h2>
                     </div>
                     <div className="vid-box">
-                        <img className="vid-box-player" width={400} height={200} />
+                        <img src={videoPlaceholder} className="vid-box-player" width={400} height={200} />
                         <h2 className="vid-box-title">Title no 1</h2>
                     </div>
                     <div className="vid-box">
-                        <img className="vid-box-player" width={400} height={200} />
+                        <img src={videoPlaceholder} className="vid-box-player" width={400} height={200} />
                         <h2 className="vid-box-title">Title no 1</h2>
                     </div>
                 </div>
