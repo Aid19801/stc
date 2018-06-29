34dea406147521eb2db7d6a8ba0edab237d0aa67
Adrian Thompson <adrian.thompson@sky.uk>
2018-06-29 08:31:36 +0100

    css bounce of image panes

--- src/App/Reuseable-Components/Pane-Box/pane-box.js
+++ src/App/Reuseable-Components/Pane-Box/pane-box.js
@@ -3,7 +3,7 @@ import videoPlaceholder from './videoPlaceholder.jpg'
 import styles from './styles.css';
 
 const PaneBox = (props) => (
-                <div key={props.key} className="paneBox-outerDiv">
+                <div key={props.key} id="pane-box-id" className="paneBox-outerDiv">
                     <img src={videoPlaceholder} className="paneBox-fineBorder" width={400} height={200} />
                     <h2 className="paneBox-title">{props.eachVideo.title}</h2>
                     <h4 className="paneBox-tagline">{props.eachVideo.tagline}</h4>
